da2db7397e7e85172b8bc252fa329a9e0e50501f
sungenyeint <sungenyeint.gmail.com>
2023-10-16 16:00:26 +0630

    fix id NG

--- src/App.js
+++ src/App.js
@@ -7,44 +7,43 @@ function App() {
 const [taskItem, setTaskItem] = useState("");
 const [taskListArr, setTaskListArr] = useState([
     {
-        id : 1695378061158,
+        id : 1,
         value : 'テスト①',
         isDone : true
     },
     {
-        id : 1695378061158,
+        id : 2,
         value : 'テスト②',
         isDone : false
     },
     {
-        id : 1695378061158,
+        id : 3,
         value : 'テスト③',
         isDone : true
     },
 ]);
 
 const addTodoItem = (todoValue, e) => {
-    console.log(taskListArr);
     e.preventDefault();
     let val = todoValue.replace(/[^a-zA-Z0-9- ]/g, "");
     if (val.trim() !== "") {
-    const newItem = {
-        id: Date.now(),
-        value: val,
-        isDone: false,
-    };
-    const todoList = [...taskListArr];
-    todoList.push(newItem);
-    setTaskItem("");
-    setTaskListArr(todoList);
+        const newItem = {
+            id: taskListArr[taskListArr.length - 1].id + 1,
+            value: val,
+            isDone: false,
+        };
+        const todoList = [...taskListArr];
+        todoList.push(newItem);
+        setTaskItem("");
+        setTaskListArr(todoList);
     } else {
-    setTaskItem("");
-    Swal.fire({
-        icon: "error",
-        title: "Invalid or Empty Task",
-        focusConfirm: false,
-        confirmButtonColor: "#2b2d5d",
-    });
+        setTaskItem("");
+        Swal.fire({
+            icon: "error",
+            title: "Invalid or Empty Task",
+            focusConfirm: false,
+            confirmButtonColor: "#2b2d5d",
+        });
     }
 };
 
@@ -67,71 +66,71 @@ const handleCheck = (isChecked, taskId) => {
 
 return (
     <div>
-    <div className="container">
-        <form className="frmMrgn">
-        <div className="form-group">
-            <h1 className="text-center">Todoアプリ</h1>
-            <div className="input-group-prepend row">
-                <div className="col-9">
-                    <input
-                        type="search"
-                        className="form-control"
-                        placeholder="何をするか？"
-                        required
-                        value={taskItem}
-                        onChange={(e) => updateInput(e.target.value)}
-                    />
+        <div className="container">
+            <form className="frmMrgn">
+                <div className="form-group">
+                    <h1 className="text-center">Todoアプリ</h1>
+                    <div className="input-group-prepend row">
+                        <div className="col-9">
+                            <input
+                                type="search"
+                                className="form-control"
+                                placeholder="何をするか？"
+                                required
+                                value={taskItem}
+                                onChange={(e) => updateInput(e.target.value)}
+                            />
+                        </div>
+                        <div className="col-3" style={{ marginTop: '6px'}}>
+                            <button
+                                className="btn btn-primary form-control"
+                                onClick={(e) => addTodoItem(taskItem, e)}
+                                disabled={!taskItem.length}
+                            >
+                                追加
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <div className="col-3" style={{ marginTop: '6px'}}>
-                    <button
-                        className="btn btn-primary form-control"
-                        onClick={(e) => addTodoItem(taskItem, e)}
-                        disabled={!taskItem.length}
-                    >
-                        追加
-                    </button>
-                </div>
+            </form>
+
+            <div className="cardParent mt-5">
+                <ul className="list-group">
+                    {taskListArr.map((item) => {
+                    return (
+                        <li
+                        key={item.id}
+                        className="list-group-item d-flex  centerAlign lstItem"
+                        >
+                        <input
+                            type="checkbox"
+                            name="isDone"
+                            checked={item.isDone}
+                            onChange={(e) => {
+                            handleCheck(e.target.checked, item.id);
+                            }}
+                        />
+                        <div className="tt">{item.value}</div>
+                        <span
+                            className={`badge badge-pill customBdg ${
+                            item.isDone === false ? "bg-danger" : "bg-success"
+                            }`}
+                        >
+                            {`${item.isDone === false ? `保留中` : "完了しました"}`}
+                        </span>
+                        <button
+                            className="btn btn-danger buttonCustom"
+                            disabled={!item.isDone}
+                            onClick={() => deleteTodoItem(item.id)}
+                        >
+                            削除
+                        </button>
+                        </li>
+                    );
+                    })}
+                </ul>
             </div>
         </div>
-        </form>
-
-        <div className="cardParent">
-        <ul className="list-group">
-            {taskListArr.map((item) => {
-            return (
-                <li
-                key={item.id}
-                className="list-group-item d-flex  centerAlign lstItem"
-                >
-                <input
-                    type="checkbox"
-                    name="isDone"
-                    checked={item.isDone}
-                    onChange={(e) => {
-                    handleCheck(e.target.checked, item.id);
-                    }}
-                />
-                <div className="tt">{item.value}</div>
-                <span
-                    className={`badge badge-pill customBdg ${
-                    item.isDone === false ? "bg-danger" : "bg-success"
-                    }`}
-                >
-                    {`${item.isDone === false ? `保留中` : "完了しました"}`}
-                </span>
-                <button
-                    className="btn btn-danger buttonCustom"
-                    disabled={!item.isDone}
-                    onClick={() => deleteTodoItem(item.id)}
-                >
-                    削除
-                </button>
-                </li>
-            );
-            })}
-        </ul>
-        </div>
-    </div>
     </div>
 );
 }
