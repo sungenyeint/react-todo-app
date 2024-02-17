8f4284b7ec950fea560e6128ae6120050ebe51e6
sungenyeint <sungenyeint@gmail.com>
2024-02-17 21:53:54 +0630

    update

--- src/App.js
+++ src/App.js
@@ -1,138 +1,152 @@
 import React, { Component, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Swal from "sweetalert2";
 
-function App() {
-const [taskItem, setTaskItem] = useState("");
-const [taskListArr, setTaskListArr] = useState([
-    {
-        id : 1,
-        value : 'テスト①',
-        isDone : true
-    },
-    {
-        id : 2,
-        value : 'テスト②',
-        isDone : false
-    },
-    {
-        id : 3,
-        value : 'テスト③',
-        isDone : true
-    },
-]);
+function TodoNew({addItem}) {
+    const [inputItem, setInputItem] = useState('');
 
-const addTodoItem = (todoValue, e) => {
-    e.preventDefault();
-    let val = todoValue.replace(/[^a-zA-Z0-9- ]/g, "");
-    if (val.trim() !== "") {
-        const newItem = {
-            id: taskListArr[taskListArr.length - 1].id + 1,
-            value: val,
-            isDone: false,
-        };
-        const todoList = [...taskListArr];
-        todoList.push(newItem);
-        setTaskItem("");
-        setTaskListArr(todoList);
-    } else {
-        setTaskItem("");
-        Swal.fire({
-            icon: "error",
-            title: "Invalid or Empty Task",
-            focusConfirm: false,
-            confirmButtonColor: "#2b2d5d",
-        });
-    }
-};
+    const addHandler = () => {
+        addItem(inputItem);
+        setInputItem('');
+    };
 
-const deleteTodoItem = (delTaskId) => {
-    const todoList = [...taskListArr];
-    const updatedtodoList = todoList.filter((item) => item.id !== delTaskId);
-    setTaskListArr(updatedtodoList);
-};
-
-const updateInput = (inputTask) => {
-    setTaskItem(inputTask);
-};
-
-const handleCheck = (isChecked, taskId) => {
-    let todoList = [...taskListArr];
-    const foundItemIndex = todoList.findIndex((item) => item.id === taskId);
-    todoList[foundItemIndex].isDone = isChecked;
-    setTaskListArr(todoList);
-};
-
-return (
-    <div>
-        <div className="container">
-            <form className="frmMrgn">
-                <div className="form-group">
-                    <h1 className="text-center">Todoアプリ</h1>
-                    <div className="input-group-prepend row">
-                        <div className="col-9">
-                            <input
-                                type="search"
-                                className="form-control"
-                                placeholder="何をするか？"
-                                required
-                                value={taskItem}
-                                onChange={(e) => updateInput(e.target.value)}
-                            />
-                        </div>
-                        <div className="col-3" style={{ marginTop: '6px'}}>
-                            <button
-                                className="btn btn-primary form-control"
-                                onClick={(e) => addTodoItem(taskItem, e)}
-                                disabled={!taskItem.length}
-                            >
-                                追加
-                            </button>
-                        </div>
-                    </div>
+    return <div className="cardParent px-5">
+            <h1 className="text-center mb-5">Todoアプリ</h1>
+            <div className="input-group-prepend row">
+                <div className="col-9">
+                    <input
+                        type="search"
+                        className="form-control"
+                        placeholder="何をするか？"
+                        value={inputItem}
+                        onChange={(e) => setInputItem(e.target.value)}
+                        required />
                 </div>
-            </form>
-
-            <div className="cardParent mt-5">
-                <ul className="list-group">
-                    {taskListArr.map((item) => {
-                    return (
-                        <li
-                        key={item.id}
-                        className="list-group-item d-flex  centerAlign lstItem"
-                        >
-                        <input
-                            type="checkbox"
-                            name="isDone"
-                            checked={item.isDone}
-                            onChange={(e) => {
-                            handleCheck(e.target.checked, item.id);
-                            }}
-                        />
-                        <div className="tt">{item.value}</div>
-                        <span
-                            className={`badge badge-pill customBdg ${
-                            item.isDone === false ? "bg-danger" : "bg-success"
-                            }`}
-                        >
-                            {`${item.isDone === false ? `保留中` : "完了しました"}`}
-                        </span>
-                        <button
-                            className="btn btn-danger buttonCustom"
-                            disabled={!item.isDone}
-                            onClick={() => deleteTodoItem(item.id)}
-                        >
-                            削除
-                        </button>
-                        </li>
-                    );
-                    })}
-                </ul>
+                <div className="col-3">
+                    <button
+                        className="btn btn-primary form-control"
+                        onClick={addHandler}
+                    >
+                        追加
+                    </button>
+                </div>
             </div>
-        </div>
-    </div>
-);
+        </div>;
 }
 
-export default App;
+function TodoItem({item, onDelete, onUpdate}) {
+    const [editMode, setEditMode] = useState(false);
+    const [editValue, setEditValue] = useState(item.value);
+
+    const checkHandler = (e) => {
+        const updateItem = {...item, isDone: ! item.isDone};
+        onUpdate(updateItem);
+    };
+
+    const deleteHandler = () => {
+        onDelete(item);
+    };
+
+    const editHandler = () => {
+        if (editMode === true) {
+            const updateItem = {...item, value: editValue};
+            onUpdate(updateItem);
+        }
+        setEditMode (! editMode);
+    }
+
+    return <li
+        key={item.id}
+        className="list-group-item d-flex  centerAlign lstItem"
+    >
+        <input
+            type="checkbox"
+            name="isDone"
+            checked={item.isDone}
+            disabled={editMode}
+            onChange={checkHandler}/>
+        <div className="tt">
+            {
+                editMode ?
+                <input
+                    type="text"
+                    value={editValue}
+                    onChange={(e) => setEditValue(e.target.value)}/> :
+                item.value
+            }
+        </div>
+        <span
+            className={`badge badge-pill customBdg ${item.isDone === false ? "bg-danger" : "bg-success"}`}
+        >
+            {`${item.isDone === false ? `保留中` : "完了しました"}`}
+        </span>
+        <button
+            className="btn btn-danger buttonCustom"
+            disabled={!item.isDone}
+            onClick={deleteHandler}
+        >
+            削除
+        </button>
+        <button
+            className="btn btn-primary buttonCustom"
+            disabled={!item.isDone}
+            onClick={editHandler}
+        >
+            {editMode ? 'Update' : 'Edit'}
+        </button>
+    </li>;
+}
+
+export default function App() {
+    const [taskListArr, setTaskListArr] = useState([
+        {
+            id : 1,
+            value : 'テスト①',
+            isDone : true
+        },
+        {
+            id : 2,
+            value : 'テスト②',
+            isDone : false
+        },
+        {
+            id : 3,
+            value : 'テスト③',
+            isDone : true
+        },
+    ]);
+
+    const addHandler = (value) => {
+        console.log(value);
+        const newItem = {
+            id : taskListArr.length + 1,
+            value : value,
+            isDone : false
+        };
+        setTaskListArr([...taskListArr, newItem]);
+    };
+
+    const deleteHandler = (item) => {
+        setTaskListArr(taskListArr.filter(todo=> todo.id != item.id));
+    };
+
+    const updateHandler = (item) => {
+        setTaskListArr(taskListArr.map(todo=> todo.id == item.id ? item : todo));
+    };
+
+    return (
+        <div>
+            <div className="container">
+                <TodoNew addItem={addHandler}/>
+                <div className="cardParent mt-5">
+                    <ul className="list-group">
+                        {taskListArr.map(item => <TodoItem key={item.id}
+                                                            onDelete={deleteHandler}
+                                                            onUpdate={updateHandler}
+                                                            item={item}/>)}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+}
